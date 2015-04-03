f5fbc2caf9f5bfc146f230b77d0e07197ce9eca0
Christian Javellana <cjjavellana@gmail.com>
2015-04-03 23:28:41 +0800

    Added membership fee forms

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,29 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def membership
+    # Try to get list of countries from the cache
+    @countries = $redis.get('countries')
+    if @countries.nil?
+      # A cache-miss, retrieve it from db
+      @countries = Country.all.to_json
+      $redis.set('countries', @countries)
+    else
+      # A cache-hit, since redis can only store strings
+      # We serialize active record to json when storing
+      # and convert from json when retrieving from redis
+      @countries = JSON.parse(@countries)
+    end
+
+    @cc_types = $redis.get('cc_types')
+    if @cc_types.nil?
+      @cc_types = CreditCardType.all.to_json
+      $redis.set('cc_types', @cc_types)
+    else
+      @cc_types = JSON.parse(@cc_types)
+    end
+
+    @form = MembershipForm.new
+
   end
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/registrations/confirm_account' => 'registrations#confirm_account'
     get '/users/registrations/membership' => 'registrations#membership'
+    post '/users/registrations/membership' => 'registrations#process_membership_fee'
   end
 
   get '/shop/browse', to: 'main#index'
+
+
 end
