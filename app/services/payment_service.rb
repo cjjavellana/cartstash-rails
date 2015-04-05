04ea024e6023c85fb594376da5eda3c483ec2127
Christian Javellana <cjjavellana@gmail.com>
2015-04-05 17:18:20 +0800

    Refactored payment request creation and added unit tests

--- app/services/payment_service.rb
+++ app/services/payment_service.rb
@@ -5,24 +5,44 @@ class PaymentService
   include PayPal::SDK::REST
   include CartstashError
 
-  def credit_card_pay(payment_form, purchased_items, transaction_id, currency='PHP')
-    
+  def charge_credit_card(payment_form, purchased_items, transaction_id, currency='PHP')
+    payment_request = PaymentRequestHelper.new payment_form, purchased_items, transaction_id, currency
+    @payment = Payment.new(payment_request.create_payment_request)
+
+    if @payment.create
+      @payment.id
+    else
+      raise PaymentError, @payment.error
+    end
+  end
+end
+
+class PaymentRequestHelper
+
+  def initialize(payment_form, purchased_items, transaction_id, currency='PHP')
+    @payment_form = payment_form
+    @purchased_items = purchased_items
+    @transaction_id = transaction_id
+    @currency = currency
+  end
+
+  def create_payment_request
     billing_address = {
-        :line1 => payment_form.address_line_1,
-        :line2 => payment_form.address_line_2,
-        :city => payment_form.city,
-        :country_code => payment_form.country,
-        :postal_code => payment_form.zip_code
+        :line1 => @payment_form.address_line_1,
+        :line2 => @payment_form.address_line_2,
+        :city => @payment_form.city,
+        :country_code => @payment_form.country,
+        :postal_code => @payment_form.zip_code
     }
 
     credit_card = {
-        :type => payment_form.card_type,
-        :number => payment_form.credit_card_no,
-        :expire_month => payment_form.expiry_month,
-        :expire_year => payment_form.expiry_year,
-        :cvv2 => payment_form.security_code,
-        :first_name => payment_form.first_name,
-        :last_name => payment_form.last_name,
+        :type => @payment_form.card_type,
+        :number => @payment_form.credit_card_no,
+        :expire_month => @payment_form.expiry_month,
+        :expire_year => @payment_form.expiry_year,
+        :cvv2 => @payment_form.security_code,
+        :first_name => @payment_form.first_name,
+        :last_name => @payment_form.last_name,
         :billing_address => billing_address
     }
 
@@ -36,17 +56,17 @@ class PaymentService
     total_amount = 0;
 
     # Construct the items list and calculate the relevant charges i.e. discount, total amount
-    purchased_items.each do |item|
+    @purchased_items.each do |item|
       items.push({
-                       :name => item.name,
-                       :sku => item.sku,
-                       :price => item.price.to_s,
-                       :currency => currency,
-                       :quantity => item.quantity
-                   })
+                     :name => item.name,
+                     :sku => item.sku,
+                     :price => item.price.to_s,
+                     :currency => @currency,
+                     :quantity => item.quantity
+                 })
       total_amount += item.price
       if item.discount
-        discount_amount += item.price - (item.price * item.discount)
+        discount_amount += (item.price * item.discount)
       end
     end
 
@@ -55,35 +75,31 @@ class PaymentService
                        :name => 'Discount',
                        :sku => 'discount',
                        :price => discount_amount.to_s,
-                       :currency => currency,
+                       :currency => @currency,
                        :quantity => "1"
                    })
     end
 
     amount = {
         :total => (total_amount - discount_amount).to_s,
-        :currency => currency
+        :currency => @currency
     }
 
-    @payment = Payment.new({
-                               :intent => "sale",
-                               :payer => payer,
-                               :transactions => [
-                                   {
-                                       :items_list => {
-                                           :items => items
-                                       },
-                                       :amount => amount,
-                                       :description => "Payment for transaction #{transaction_id}"
-                                   }
-                               ]
-                           })
-
-    if @payment.create
-      @payment.id
-    else
-      raise PaymentError, @payment.error
-    end
+    {
+        :intent => "sale",
+        :payer => payer,
+        :transactions => [
+            {
+                :items_list => {
+                    :items => items
+                },
+                :amount => amount,
+                :description => "Payment for transaction #{@transaction_id}"
+            }
+        ]
+    }
   end
-end
 
+  private
+
+end
--- spec/service/payment_service_spec.rb
+++ spec/service/payment_service_spec.rb
@@ -3,29 +3,47 @@ require 'rails_helper'
 describe PaymentService do
 
   context "Payment" do
+    before(:each) do
+      @payment_form = CreditCardPaymentForm.new(:card_type => 'visa', :credit_card_no => '4539016690974009',
+                                               :expiry_date => '10/2015', :first_name => 'John', :last_name => 'Doe',
+                                               :security_code => '578', :address_line_1 => '#123-456 Make Believe Blvd',
+                                               :city => 'Unknown City', :zip_code => '5000', :country => 'PH')
+      @items = []
+      purchased_item = PurchasedItem.new
+      purchased_item.name = 'Test 001'
+      purchased_item.sku = '001'
+      purchased_item.price = 100
+      purchased_item.quantity = 10
+      @items.push(purchased_item)
+    end
 
-    it "must return payment id" do
-      payment_form = CreditCardPaymentForm.new(:card_type => 'visa', :credit_card_no => '4539016690974009',
-                                                :expiry_date => '10/2015', :first_name => 'John', :last_name => 'Doe',
-                                                :security_code => '578', :address_line_1 => '#123-456 Make Believe Blvd',
-                                                :city => 'Unknown City', :zip_code => '5000', :country => 'PH')
-      if payment_form.valid?
-        items = []
-        purchased_item = PurchasedItem.new
-        purchased_item.name = 'Test 001'
-        purchased_item.sku = '001'
-        purchased_item.price = 100
-        purchased_item.quantity = 10
+    it "is able to construct the payment request" do
+      if @payment_form.valid?
+        payment_helper = PaymentRequestHelper.new @payment_form, @items, 'Unit Test', 'USD'
+        expect(payment_helper.create_payment_request[:transactions][0][:amount][:total]).to eq(100.to_s)
+      end
+    end
 
-        items.push(purchased_item)
+    it "is able to apply discount" do
+      @items.each do |item|
+        # Simulate a 10% discount
+        item.discount = 0.10
+      end
 
+      if @payment_form.valid?
+        payment_helper = PaymentRequestHelper.new @payment_form, @items, 'Unit Test', 'USD'
+        expect(payment_helper.create_payment_request[:transactions][0][:amount][:total]).to eq(90.0.to_s)
+      end
+    end
+
+    it "is able to process the transaction and return the payment id" do
+      if @payment_form.valid?
         payment_service = PaymentService.new
-        payment_id = payment_service.credit_card_pay payment_form, items, 'Unit Test', 'USD'
+        payment_id = payment_service.charge_credit_card @payment_form, @items, 'Unit Test', 'USD'
         print "Payment Id: #{payment_id}"
         expect(payment_id).to_not eq(nil)
       end
     end
-
   end
 
 end
