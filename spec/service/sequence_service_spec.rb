40d9eab7fa08dc24b7cffc7105d5c30f41223255
Christian Javellana <cjjavellana@gmail.com>
2015-04-22 09:09:51 +0800

    Removed code repetition

--- spec/service/sequence_service_spec.rb
+++ spec/service/sequence_service_spec.rb
@@ -11,39 +11,30 @@ describe SeqGenerator do
     end
 
     it "succeeds when there is no duplicate" do
-      seq_generator = SeqGenerator.instance
-      sequences = []
-      threads = (1..5).map do |i|
-        Thread.new(i) do
-          val = []
-          (1..10).each do |j|
-            type = ((j % 2) == 0) ? "payment" : "membership"
-            seq = seq_generator.generate_sequence type
-            val.push("#{type}-#{seq}")
-          end
-          sequences.push(val)
-        end # thread.new
-      end # (1..5).map
-
-      threads.each do |t|
-        t.join
-      end # threads.each
-
-      final_seq_list = []
-      sequences.each do |seq|
-        final_seq_list.concat seq
-      end
+      seq_list = generate_sequences
 
       # Ensure that we have generated 50 items
-      expect(final_seq_list.length).to eq(50)
+      expect(seq_list.length).to eq(50)
 
       # Ensure that there is no duplicate
-      result = final_seq_list.detect { |e| final_seq_list.rindex(e) != final_seq_list.index(e) }
+      result = seq_list.detect { |e| seq_list.rindex(e) != seq_list.index(e) }
       expect(result).to be_nil
 
     end # it ... do
 
     it "fails when there is a duplicate" do
+      seq_list = generate_sequences
+
+      #disrupt the sequences by inserting a duplicate
+      seq_list.push(seq_list[seq_list.length - 1])
+
+      result = seq_list.detect { |e| seq_list.rindex(e) != seq_list.index(e) }
+      expect(result).to_not be_nil
+    end
+  end
+
+  private
+    def generate_sequences
       seq_generator = SeqGenerator.instance
       sequences = []
       threads = (1..5).map do |i|
@@ -67,12 +58,7 @@ describe SeqGenerator do
         final_seq_list.concat seq
       end
 
-      #disrupt the sequences by inserting a duplicate
-      final_seq_list.push(final_seq_list[final_seq_list.length - 1])
-
-      result = final_seq_list.detect { |e| final_seq_list.rindex(e) != final_seq_list.index(e) }
-      expect(result).to_not be_nil
+      final_seq_list
     end
-  end
 
 end
