811864060b8a5dd3b1f2cbbc46f840a3e6838763
Christian Javellana <cjjavellana@gmail.com>
2015-05-05 09:00:38 +0800

    Added comment

--- app/models/redis_client.rb
+++ app/models/redis_client.rb
@@ -4,10 +4,21 @@ class RedisClient
     $redis.set key, value
   end
 
+  #
+  # Stores a key-value pair into redis that will expire after a specified
+  # amount of time.
+  #
+  # @key - A string, the cache key
+  # @value - A string, the value to to store
+  # @expiry - A fixnum, default of 86400 (1 day)
   def self.set_with_expiry(key, value, expiry = 86400)
     $redis.set(key, value, {:ex => expiry})
   end
 
+  #
+  # Retrieves a value from the cache
+  #
+  # @key - A string, the key which identifies the value to retrieve
   def self.get(key)
     $redis.get key
   end
