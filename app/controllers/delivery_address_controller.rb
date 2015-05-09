1aa621c68a95fb983b38de1b5eb3b0d00246f73e
Christian Javellana <cjjavellana@gmail.com>
2015-05-09 10:26:35 +0800

    Initial work for manage delivery addresses

--- app/controllers/delivery_address_controller.rb
+++ app/controllers/delivery_address_controller.rb
@@ -1,2 +1,8 @@
+# Allows the user to add / update / remove configured delivery addresses
 class DeliveryAddressController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  	@delivery_addresses = DeliveryAddress.where("user_id = ? and status = ?", current_user.id, "active")
+  end
 end
--- spec/controllers/delivery_address_controller_spec.rb
+++ spec/controllers/delivery_address_controller_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe DeliveryAddressController, type: :controller do
 
+  describe "Manage Configured Delivery Addresses" do
+    login_user
+
+    let(:delivery_address) {
+      build_stubbed(:foobar_delivery_address)
+    }
+
+    it "displays the user's configured delivery addresses" do
+      allow(DeliveryAddress).to receive(:where).and_return([delivery_address])
+      get :index
+      expect(assigns(:delivery_addresses).length).to eq(1)
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+    end
+
+    it "allows a user to create new delivery address" do
+
+    end
+
+  end
+
 end
