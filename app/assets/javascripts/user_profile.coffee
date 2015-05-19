f2556bdbb7fbebac00afe7beffc596b9fff5d69c
Christian Javellana <cjjavellana@gmail.com>
2015-05-19 22:15:41 +0800

    Fix birthdate date picker not opening on 1st load + Fix birthdate not saving to database + set birthdate field format

--- app/assets/javascripts/user_profile.coffee
+++ app/assets/javascripts/user_profile.coffee
@@ -1,6 +1,6 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).on 'ready', ->
+$(document).on 'page:change', ->
   if $('#birthdate').length > 0
     $('#birthdate').datepicker()
--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -1,7 +1,8 @@
 class UserProfileController < ApplicationController
 
+  before_action :set_countries
+
   def index
-    @countries = Country.get_countries
     @user = User.find(current_user.id)
   end
 
@@ -10,13 +11,12 @@ class UserProfileController < ApplicationController
     @user = User.find(current_user.id)
 
     if @user.valid?
-      @user.assign_attributes(secure_params)
+      set_attributes
       @user.save
 
       flash[:notice] = "User profile successfully updated"
     end
 
-    @countries = Country.get_countries
     render :index
   end
 
@@ -24,5 +24,20 @@ class UserProfileController < ApplicationController
     def secure_params
       params.require(:user).permit(:first_name, :last_name, :gender, :birthdate,
                                    :address_line_1, :address_line_2, :zip, :country)
+    end
+
+    def set_attributes
+      @user.assign_attributes(secure_params)
+      unless secure_params[:birthdate].nil?
+        begin
+          @user.birthdate = Date.strptime(secure_params[:birthdate], '%m/%d/%Y')
+        rescue
+          print "Unable to convert to date #{secure_params[:birthdate]}"
+        end
+      end
+    end
+
+    def set_countries
+      @countries = Country.get_countries
     end
 end
