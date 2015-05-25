9b93a6dbbf384903371d6b3f991160b12a7ebaf2
Christian Javellana <cjjavellana@gmail.com>
2015-05-25 23:04:06 +0800

    Initial work for edit payment method

--- app/controllers/payment_method_controller.rb
+++ app/controllers/payment_method_controller.rb
@@ -5,4 +5,8 @@ class PaymentMethodController < ApplicationController
     @payment_methods = PaymentMethod.where("user_id = ? and status = ?", current_user.id, "active")
   end
 
+  def edit
+    @payment_method = PaymentMethod.where("user_id = ? and id = ?", current_user.id, params[:id]).first
+  end
+
 end
--- spec/controllers/payment_method_controller_spec.rb
+++ spec/controllers/payment_method_controller_spec.rb
@@ -5,15 +5,24 @@ RSpec.describe PaymentMethodController, type: :controller do
   describe "Payment method" do
     login_user
 
+    let(:visa) {
+      build_stubbed(:foobar_visa)
+    }
+
     it "displays the user's payment methods" do
-      expect(PaymentMethod).to receive(:where).and_return(build_stubbed(:foobar_visa))
+      expect(PaymentMethod).to receive(:where).and_return(visa)
 
       get :index
       expect(assigns(:payment_methods)).to_not be_nil
       expect(response).to have_http_status(:success)
     end
+
+    it "displays the payment method edit page" do
+      expect(PaymentMethod).to receive(:where).and_return(visa)
+
+      get :edit, id: visa.id
+      expect(assigns(:payment_method).first_name).to eq('Foobar')
+      expect(response).to have_http_status(:success)
+    end
   end
-
-
-
 end
