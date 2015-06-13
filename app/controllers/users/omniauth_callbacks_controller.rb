08300b3f335d9c7a247c2d069a1abfc63f853fca
Christian Javellana <cjjavellana@gmail.com>
2015-06-13 10:56:22 +0800

    Scoped the omniauth callback controller under users to prevent circular loading dependency + redirect to shop upon successful omniauth login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class OmniAuthCallbacksController < Devise::OmniAuthCallbacksControllerController
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def self.provides_callback_for(provider)
     class_eval %Q{
@@ -6,8 +6,9 @@ class OmniAuthCallbacksController < Devise::OmniAuthCallbacksControllerControlle
         @user = User.find_for_oauth(env["omniauth.auth"], current_user)
 
         if @user.persisted?
-          sign_in_and_redirect @user, event: :authentication
+          sign_in @user, event: :authentication
           set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+          redirect_to '/shop'
         else
           session["devise.#{provider}_data"] = env["omniauth.auth"]
           redirect_to new_user_registration_url
