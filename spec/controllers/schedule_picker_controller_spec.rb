a470bf7c9f3380af0f394206ead9ed9be2033355
Christian Javellana <cjjavellana@gmail.com>
2015-07-15 08:13:51 +0800

    Removed unit test for method that have been removed

--- spec/controllers/schedule_picker_controller_spec.rb
+++ spec/controllers/schedule_picker_controller_spec.rb
@@ -3,11 +3,7 @@ require 'rails_helper'
 RSpec.describe SchedulePickerController, type: :controller do
 
   describe "get" do
-    it "retrieves the current date and time from the server" do
-      xhr :get, :current_datetime
-      expect(response).to have_http_status(:success)
-    end
-
+    
     it "returns an error when parameter date is less than current date" do
       expected_response = {error: "Date cannot be less than current date"}.to_json
 
